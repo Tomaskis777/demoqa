5df28028421b5291fb688a9a639333eec1b85216
Tom <mr.ivan.ivanovich777@mail.ru>
2024-06-15 16:13:51 +0300

    Firs commit

--- tests/test_check_icon_demoqa.py
+++ tests/test_check_icon_demoqa.py
@@ -1,13 +1,20 @@
-from selenium.webdriver.common.by import By
-from pages.demoqa import DemoQa
-import time
-
-def test_check_icon(browser):
-
-    demo_qa_page = DemoQa(browser)
-    demo_qa_page.visit()
-    time.sleep(3)
-    demo_qa_page.click_on_the_icon()
-    time.sleep(3)
-    assert demo_qa_page.equal_url()
-    assert demo_qa_page.exist_icon()
+# # from pages.demoqa import DemoQa
+# # import time
+# from pages.elements_page import ElementsPage
+#
+# # def test_icon_exist(browser):
+# #
+# #     demo_qa_page = DemoQa(browser)
+# #     demo_qa_page.visit()
+# #     time.sleep(3)
+# #     demo_qa_page.icon.click()
+# #     time.sleep(3)
+# #     assert demo_qa_page.equal_url()
+# #     assert demo_qa_page.icon.exist()
+#
+#
+# def test_page_elements(browser):
+#     el_page = ElementsPage(browser)
+#
+#     el_page.visit()
+#     assert el_page.text_elements.get_text == 'Please select an item from left to start practice.'
