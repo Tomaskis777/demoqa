8151a21d701ffa4813a2ca57e4addd34de7d4064
Tom <mr.ivan.ivanovich777@mail.ru>
2024-06-19 00:25:57 +0300

    Firs commit

--- tests/test_visible.py
+++ tests/test_visible.py
@@ -7,11 +7,11 @@ import time
 #     elements_page.visit()
 #     assert elements_page.btn_sidebar_textbox.visible()
 
-def test_not_visible_btn_sidebar(browser):
-    elements_page = ElementsPage(browser)
-
-    elements_page.visit()
-    assert elements_page.btn_sidebar_checkbox.visible()
-    elements_page.btn_sidebar_first.click()
-    time.sleep(2)
-    assert not elements_page.btn_sidebar_checkbox.visible()
+# def test_not_visible_btn_sidebar(browser):
+#     elements_page = ElementsPage(browser)
+#
+#     elements_page.visit()
+#     assert elements_page.btn_sidebar_checkbox.visible()
+#     elements_page.btn_sidebar_first.click()
+#     time.sleep(2)
+#     assert not elements_page.btn_sidebar_checkbox.visible()
