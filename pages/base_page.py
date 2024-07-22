1ab5795e3624f2e2c8a46295f71de01c2e1ee38c
Tom <mr.ivan.ivanovich777@mail.ru>
2024-07-22 19:22:48 +0300

    Firs coit

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,12 +1,13 @@
-from selenium.webdriver.common.by import By
 import logging
+from components.components import WebElement
+
 
 class BasePage:
-
 
     def __init__(self, driver, base_url):
         self.driver = driver
         self.base_url = base_url
+        self.viewport = WebElement(driver, 'head > meta:nth-child(2)')
 
     def visit(self):
         return self.driver.get(self.base_url)
@@ -36,5 +37,5 @@ class BasePage:
         try:
             return self.driver.switch_to.alert
         except Exception as ex:
-            logging.log(1,ex)
+            logging.log(1, ex)
             return False
