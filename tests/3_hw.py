4fa019dff066c7e7b2791ea691c919806326e16e
Tom <mr.ivan.ivanovich777@mail.ru>
2024-08-26 22:18:50 +0300

    Firs coit

--- tests/3_hw.py
+++ tests/3_hw.py
@@ -1,44 +1,44 @@
-# def max_num(a=120, b=180):
-#     return max(a, b)
-#
-#
-# print(max_num())
-#
-#
-# def check_difference(num1, num2):
-#     if abs(num1-num2) == 135:
-#         return "Yes"
-#     else:
-#         return "No"
-#
-# results = check_difference(255, 120)
-# print(results)
+def max_num(a=120, b=180):
+    return max(a, b)
 
 
-# def year_seasons(season_of_year):
-#     if season_of_year == 12 or season_of_year == 1 or season_of_year == 2:
-#         print('Зима')
-#     elif 3 <= season_of_year <= 5:
-#         print('Весна')
-#     elif 6 <= season_of_year <= 8:
-#         print('Лето')
-#     elif 9 <= season_of_year <= 11:
-#         print('Осень')
-#     else:
-#         print('Введите корректный месяц')
-#
-#
-# year_seasons(1)
+print(max_num())
 
 
-# def another_number(a=15, b=25, c=55):
-#     # return (a, b, c)
-#     if (a, b, c) ==> 10:
-#         return "Yes"
-#     else:
-#         return "No"
-#
-# print(results)
+def check_difference(num1, num2):
+    if abs(num1-num2) == 135:
+        return "Yes"
+    else:
+        return "No"
+
+results = check_difference(255, 120)
+print(results)
+
+
+def year_seasons(season_of_year):
+    if season_of_year == 12 or season_of_year == 1 or season_of_year == 2:
+        print('Зима')
+    elif 3 <= season_of_year <= 5:
+        print('Весна')
+    elif 6 <= season_of_year <= 8:
+        print('Лето')
+    elif 9 <= season_of_year <= 11:
+        print('Осень')
+    else:
+        print('Введите корректный месяц')
+
+
+year_seasons(1)
+
+
+def another_number(a=15, b=25, c=55):
+    # return (a, b, c)
+    if (a, b, c) ==> 10:
+        return "Yes"
+    else:
+        return "No"
+
+print(results)
 
 
 def check_numbers(num1, num2, num3):
