327dfcbaa1f6a05cf38a3554f82bdddc6a29c382
Tom <mr.ivan.ivanovich777@mail.ru>
2024-08-26 22:15:46 +0300

    Firs coit

--- tests/3_hw.py
+++ tests/3_hw.py
@@ -3,21 +3,50 @@
 #
 #
 # print(max_num())
-
-
-# def check_difference(num1=255, num2=120):
-#     return num1, num2
 #
+#
+# def check_difference(num1, num2):
 #     if abs(num1-num2) == 135:
-#         print("Yes")
+#         return "Yes"
 #     else:
-#         print("No")
+#         return "No"
+#
+# results = check_difference(255, 120)
+# print(results)
 
-def check_difference(num1, num2):
-    if abs(num1-num2) == 135:
-        return "Yes"
+
+# def year_seasons(season_of_year):
+#     if season_of_year == 12 or season_of_year == 1 or season_of_year == 2:
+#         print('Зима')
+#     elif 3 <= season_of_year <= 5:
+#         print('Весна')
+#     elif 6 <= season_of_year <= 8:
+#         print('Лето')
+#     elif 9 <= season_of_year <= 11:
+#         print('Осень')
+#     else:
+#         print('Введите корректный месяц')
+#
+#
+# year_seasons(1)
+
+
+# def another_number(a=15, b=25, c=55):
+#     # return (a, b, c)
+#     if (a, b, c) ==> 10:
+#         return "Yes"
+#     else:
+#         return "No"
+#
+# print(results)
+
+
+def check_numbers(num1, num2, num3):
+    if num1 > 10 and num2 > 10 and num3 > 10:
+        return "yes"
     else:
-        return "No"
+        return "no"
 
-result = check_difference(255, 120)
+
+result = check_numbers(15, 22, 55)
 print(result)
