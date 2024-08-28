7e4e4abc713488d59787f746f4fe114339d661a4
Tom <mr.ivan.ivanovich777@mail.ru>
2024-08-28 18:13:47 +0300

    Firs coit

--- tests/test_koup/4_hw.py
+++ tests/test_koup/4_hw.py
@@ -6,14 +6,20 @@ class Rectangle:
     def area_rectangle(self):
         return self.width * self.height
 
-
     def perimetr_rectangle(self):
         return 2 * (self.width + self.height)
 
+
 condition1 = Rectangle(5, 5)
 condition2 = Rectangle(3, 6)
+condition3 = Rectangle(20, 35)
+
 
 print(condition1.area_rectangle())
+print(condition1.perimetr_rectangle())
+
+print(condition2.area_rectangle())
 print(condition2.perimetr_rectangle())
 
-
+print(condition3.area_rectangle())
+print(condition3.perimetr_rectangle())
