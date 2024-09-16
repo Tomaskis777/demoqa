636f176c8ce2ec3ee1725183dd2b29f82c8d7c16
Tom <mr.ivan.ivanovich777@mail.ru>
2024-09-16 18:19:05 +0300

    Firs coit

--- tests/test_hw/test_login_form.py
+++ tests/test_hw/test_login_form.py
@@ -1,38 +1,37 @@
-# import time
 import time
 
 from selenium.webdriver import Keys
 
 from pages.form_page import FormPage
 
-# def test_login_form(browser):
-#     form_page = FormPage(browser)
-#
-#     form_page.visit()
-#     assert not form_page.modal_dialog.exist()
-#     time.sleep(2)
-#     form_page.first_name.send_keys('tester')
-#     form_page.last_name.send_keys('testerovich')
-#     form_page.user_email.send_keys('test@ttt.tt')
-#     form_page.gender_radio_1.click_force()
-#     form_page.user_number.send_keys('1111111111')
-#     form_page.hobbies.click_force()
-#     form_page.current_address.send_keys('text')
-#     time.sleep(2)
-#     form_page.btn_submit.click_force()
-#     time.sleep(2)
-#
-#     assert form_page.modal_dialog.exist()
-#     form_page.btn_close_modal.click_force()
+def test_login_form(browser):
+    form_page = FormPage(browser)
+
+    form_page.visit()
+    assert not form_page.modal_dialog.exist()
+    time.sleep(2)
+    form_page.first_name.send_keys('tester')
+    form_page.last_name.send_keys('testerovich')
+    form_page.user_email.send_keys('test@ttt.tt')
+    form_page.gender_radio_1.click_force()
+    form_page.user_number.send_keys('1111111111')
+    form_page.hobbies.click_force()
+    form_page.current_address.send_keys('text')
+    time.sleep(2)
+    form_page.btn_submit.click_force()
+    time.sleep(2)
+
+    assert form_page.modal_dialog.exist()
+    form_page.btn_close_modal.click_force()
 
 
-# def test_state(browser):
-#     state_page = FormPage(browser)
-#
-#     state_page.visit()
-#     time.sleep(2)
-#     state_page.btn_state.scroll_to_element()
-#     time.sleep(2)
-#     state_page.inp_state.send_keys('NCR')
-#     state_page.inp_state.clear()
-#     time.sleep(2)
+def test_state(browser):
+    state_page = FormPage(browser)
+
+    state_page.visit()
+    time.sleep(2)
+    state_page.btn_state.scroll_to_element()
+    time.sleep(2)
+    state_page.inp_state.send_keys('NCR')
+    state_page.inp_state.clear()
+    time.sleep(2)
--- tests/test_hw/test_text_box.py
+++ tests/test_hw/test_text_box.py
@@ -8,6 +8,7 @@
 #     page_box.visit()
 #     page_box.full_name.send_keys('Mr. Full')
 #     page_box.btn_sub.click_force()
+#     time.sleep(3)
 #     assert page_box.full_name2.get_text() == 'Name:Mr. Full'
 #     page_box.full_name.clear()
 #     time.sleep(2)
