a63c75b54d0ec2b6566d632b46408084f3091299
Tom <mr.ivan.ivanovich777@mail.ru>
2024-10-03 15:57:52 +0300

    Firs coit

--- PYTHON JUN/task_4/area_visibility.py
+++ PYTHON JUN/task_4/area_visibility.py
@@ -1,10 +1,33 @@
+
+
 vid_1 = 12
 
 
-def global_func():
-    print()
-
-
-global_func()
+# def global_func():
+#     print(vid_1)
+#
+#
+# global_func()
 
 # print(vid_1)
+
+
+# def func_two():
+#     vid_1 = 'Local Value'
+#     print(vid_1)
+#
+#
+# func_two()
+#
+# print(vid_1)
+#
+# def func_three():
+#     global vid_1
+#     vid_1 = 'Local to global'
+#
+#
+# func_three()
+#
+# print(vid_1)
+
+
---
+++ PYTHON JUN/task_4/lymbda.py
@@ -0,0 +1,8 @@
+# func = lambda x: x*2
+
+
+def func_2(x):
+    return x * 2
+
+
+print(func_2(4))
