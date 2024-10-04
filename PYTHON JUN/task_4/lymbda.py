0080ee83dd668db9232b95745476562e3b3da45a
Tom <mr.ivan.ivanovich777@mail.ru>
2024-10-04 18:34:37 +0300

    Firs coit

--- PYTHON JUN/task_4/lymbda.py
+++ PYTHON JUN/task_4/lymbda.py
@@ -1,8 +1,28 @@
 # func = lambda x: x*2
 
 
-def func_2(x):
-    return x * 2
+# def func_2(x):
+#     return x * 2
+#
+#
+# print(func_2(4))
 
 
-print(func_2(4))
+# list_value = (1, 2, 3, 4, 5, 6, 7, 8, 9)
+# print(list(filter(lambda x: x % 2 == 0, list_value)))
+#
+#
+# numbers = [1, 2, 3, 4, 5, 7, 8, 10, 11]
+#
+#
+# def filter_num(in_num):
+#     return in_num % 2 == 0
+#
+#
+# print(list(filter(filter_num, numbers)))
+
+
+my_str = 'lambda expression'
+
+print(list(filter(lambda x: x != '', my_str )))
+
