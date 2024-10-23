c9ac220d2dea737d2e35c83be982f48fcd331e32
Tom <mr.ivan.ivanovich777@mail.ru>
2024-10-23 17:36:03 +0300

    Firs comit

--- PYTHON JUN/task_7/vehicle.py
+++ PYTHON JUN/task_7/vehicle.py
@@ -1,11 +1,41 @@
-class Vehicle:
-    def __init__(self, make, model, year, price):
-        self.make = make
-        self.model = model
-        self.year = year
-        self.price = price
+# class Vehicle:
+#     def __init__(self, make, model, year, price):
+#         self.make = make
+#         self.model = model
+#         self.year = year
+#         self.price = price
+#
+#     def print_info(self):
+#         print(f'Марка: {self.make}'
+#         f'\nМодель: {self.model}'
+#         f'\nГод выпуска: {self.year}'
+#         f'\nСтоимость: {self.price} руб')
+#
+#
+# class Car(Vehicle):
+#     def __init__(self, make, model, year, price, num_doors, body_style):
+#         super().__init__(make, model, year, price)
+#         self.num_doors = num_doors
+#         self.body_style = body_style
+#
+#
+# car = Car('Toyota', 'Camry', 2023, 50000, 5, 'Седан')
+#
+# car.print_info()
+#
+#
 
-def print_info(self):
-    print(f'Марка: {self.make}'
-    f'\nМодель: {self.model}'
-    f'\nГод выпуска: {self.model}'
+
+class A:
+    def method_a(self):
+        print('class a')
+
+
+class B:
+    def __init__(self):
+        self.obj_a = A()
+
+
+obj_b = B()
+obj_a = A
+print(obj_b.obj_a.method_a())
