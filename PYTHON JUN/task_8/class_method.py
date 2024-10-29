0c2b304d11206598d8397f1efafe5fe3dcdd3651
Tom <mr.ivan.ivanovich777@mail.ru>
2024-10-29 17:52:06 +0300

    Firs comit

--- PYTHON JUN/task_8/class_method.py
+++ PYTHON JUN/task_8/class_method.py
@@ -19,20 +19,27 @@ class Admin(User):
 class Teacher(User):
     def login(self):
         print(f'{self.name} - login')
+        print(f'{self.password} - password')
 
 
 class Student(User):
     pass
 
 
-admin_1 = Admin('Федор', '1236771865')
-teacher_1 = Teacher('Федор', '1236771865')
+class Magistr(Student):
+    def login(self):
+        print(f'{self.name} - login')
 
-for user in (admin_1, teacher_1):
+
+admin_1 = Admin('Федор', '1236771865')
+teacher_1 = Teacher('Anus', '1236771865')
+student_m = Magistr('Maga', '63743673')
+
+for user in (admin_1, teacher_1, student_m):
     user.login()
 
-print(admin_1.password)
-print(admin_1.name)
-print(teacher_1.name)
-print(teacher_1.password)
+# print(admin_1.password)
+# print(admin_1.name)
+# print(teacher_1.name)
+# print(teacher_1.password)
 
