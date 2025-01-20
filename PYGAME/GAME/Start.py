5c21a499ce004a579afd9aa3fd91ac53eb682246
Tom <mr.ivan.ivanovich777@mail.ru>
2025-01-20 17:41:16 +0300

    Firs comit

--- PYGAME/GAME/Start.py
+++ PYGAME/GAME/Start.py
@@ -7,10 +7,29 @@ def draw_white_circle(screen, center, radius):
     pygame.draw.circle(screen, white, center, radius)
 
 
+def draw_black_circle(screen, center, radius):
+    black = (0, 0, 0)
+    pygame.draw.circle(screen, black, center, radius)
+
+
+def draw_square(screen, size, position):
+    black = (0, 0, 0)
+    pygame.draw.rect(screen, black, (position, size))
+
+
 circles = [
     {"center": (400, 200), "radius": 50},
     {"center": (400, 300), "radius": 70},
     {"center": (400, 420), "radius": 90}
+]
+
+circles2 = [
+    {"center": (379, 200), "radius": 10},
+    {"center": (420, 200), "radius": 10},
+]
+
+squares = [
+    {"position": (60, 85), "size": (370, 75)},
 ]
 
 pygame.init()
@@ -29,6 +48,12 @@ while running:
 
     for circle in circles:
         draw_white_circle(screen, circle["center"], circle["radius"])
+
+    for circle in circles2:
+        draw_black_circle(screen, circle["center"], circle["radius"])
+
+    for rect in squares:
+        draw_square(screen, rect["position"], rect["size"])
 
     pygame.display.flip()
 
